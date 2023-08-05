6ef42eb320ca79737bc1e7c94eb09e4d63beb5f6
Hugo VERJUS <hverjus@nvidia.com>
2023-08-05 19:13:37 +0200

    addd curr

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,36 @@ use std::ptr;
 use std::sync::{Arc, Mutex};
 use sdl2::sys::SDL_EventType;
 use std::time::{Duration, Instant};
-
+use std::io::{self, Write};
+use std::string::ToString;
+use reqwest::Error;
+use sdl2::libc::time;
+use serde::{Deserialize, Serialize};
 
 const FPS: u32 = 30;
-const FRAME_TIME: Duration = Duration::from_micros((1_000_000  / FPS) as u64);
+const FRAME_TIME: Duration = Duration::from_micros((1_000_000 / FPS) as u64);
 
 const SCALE_FACTOR: u32 = 10;
 static SCREEN_WIDTH: u32 = 64 * 3 * SCALE_FACTOR;
 static SCREEN_HEIGHT: u32 = 64 * SCALE_FACTOR;
 const CUSTOM_EVENT_TYPE: u32 = SDL_EventType::SDL_USEREVENT as u32 + 1;
 
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Connection {
+    from: Departure,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Departure {
+    departure: DateTime<FixedOffset>,
+    delay: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Connections {
+    connections: Vec<Connection>,
+}
 
 // handle the annoying Rect i32
 macro_rules! rect (
@@ -31,25 +51,131 @@ macro_rules! rect (
     )
 );
 
+trait Printable {
+    fn get_text(&self) -> String;
+    fn get_color(&self) -> [u8; 3];
+}
+
 struct CustomEventData {
     // Put your custom data fields here
     message: String,
 }
 
-struct DashBoardLines {
-    line1: String,
-    line2: String,
-    line3: String,
-    line4: String,
+const URL_SBB: &str = "http://transport.opendata.ch/v1/connections?";
+
+struct URLRequest {
+    begin_station: String,
+    end_station: String,
+    fields: Vec<String>,
+    limit: u32,
 }
 
-impl DashBoardLines {
-    fn new() -> DashBoardLines {
-        DashBoardLines {
-            line1: "empty".to_string(),
-            line2: "empty".to_string(),
-            line3: "empty".to_string(),
-            line4: "empty".to_string(),
+struct URLResult {
+    timestamp: Instant,
+    delay: Duration
+}
+
+// from=Lausanne&to=Gen%C3%A8ve&fields[]=connections/from/departure&fields[]=connections/from/delay&limit=5
+struct DashBoardLine {
+    line: String,
+    color: [u8; 3],
+}
+
+struct DashBoardBusLine {
+    request_content: URLRequest,
+    result_list: Vec<URLResult>,
+    last_update: Instant,
+    color: [u8; 3],
+}
+
+
+impl DashBoardBusLine{
+    fn new(begin_station:String, end_station:String ) -> DashBoardBusLine{
+        DashBoardBusLine{
+            request_content: URLRequest{
+                begin_station,
+                end_station,
+                fields: vec![
+                    "connections/from/departure".to_string(),
+                    "connections/from/delay".to_string(),
+                ],
+                limit: 10
+            },
+            result_list: vec![],
+            last_update: Instant::now() - Duration::from_secs(3600),
+            color: [255,255,255],
+        }
+    }
+
+    async fn update(&self){
+        if self.last_update + Duration::from_secs(600) < Instant::now() {
+            return;
+        }
+        let b_sta = &self.request_content.begin_station;
+        let e_sta = &self.request_content.end_station;
+        let limit = &self.request_content.limit;
+        let mut ss = format!("{URL_SBB}from={b_sta}&to={e_sta}&limit={limit}");
+        for elm in &self.request_content.fields {
+            ss += format!("&fields[]={elm}").as_str();
+        }
+        let resa = reqwest::get(ss);
+        let res = resa.await;
+        if res.is_err(){
+            print!("Error sending GET request: {ss}");
+            return;
+        }
+        let res_text = res.unwrap().text().await;
+        if res_text.is_err(){
+            print!("Error getting text from GET request: {ss}");
+            return;
+        }
+
+        let text_to_parse = res_text.unwrap();
+        let conn_res = serde_json::from_str(text_to_parse);
+        if conn_res.is_err() {
+            return;
+
+        }
+        let conn: Connections = conn_res.unwrap();
+
+        conn.connections.map
+
+
+
+    }
+
+
+}
+
+impl Printable for DashBoardLine {
+    fn get_text(&self) -> String {
+        self.line.clone()
+    }
+    fn get_color(&self) -> [u8; 3] {
+        self.color.clone()
+    }
+}
+
+struct DashBoardPage {
+    lines: [dyn Printable; 4],
+}
+
+struct DashBoard {
+    pages: Vec<DashBoardPage>,
+}
+
+#[tokio::main]
+async fn make_request() -> Result<(), Error> {
+    let response = reqwest::get("https://google.com").await?;
+    let body = response.text().await?;
+    println!("{}", body);
+    Ok(())
+}
+
+impl DashBoard {
+    fn new() -> DashBoard {
+        DashBoard {
+            pages: vec![]
         }
     }
 
@@ -62,6 +188,17 @@ impl DashBoardLines {
             _ => { println!("Bad line number! No effect.") }
         }
     }
+
+    fn get_content(&self) -> Vec<String> {
+        let mut vec = Vec::new();
+        vec.push(self.line1.clone());
+        vec.push(self.line2.clone());
+        vec.push(self.line3.clone());
+        vec.push(self.line4.clone());
+        return vec;
+    }
+
+    fn update_content(&self) {}
 }
 
 // Scale fonts to a reasonable size when they're too big (though they might look less smooth)
@@ -91,7 +228,7 @@ fn get_centered_rect(rect_width: u32, rect_height: u32, cons_width: u32, cons_he
 
 fn printooo(
     canvas: &mut WindowCanvas,
-    lines: &[&str],
+    lines: &Vec<String>,
     font: &sdl2::ttf::Font,
     co_b: i32,
 ) {
@@ -99,11 +236,11 @@ fn printooo(
     canvas.clear();
     let texture_creator = canvas.texture_creator();
     let line_height = 160; // You might want to adjust this value
-    for (i, &line) in lines.iter().enumerate() {
+    for (i, line) in lines.iter().enumerate() {
         // render a surface, and convert it to a texture bound to the canvas
 
         let surface = font
-            .render(line)
+            .render(line.as_str())
             .blended(Color::RGBA(255, 0, 0, 255))
             .map_err(|e| e.to_string()).unwrap();
         let texture = texture_creator
@@ -133,16 +270,18 @@ fn printooo(
 
 
 fn update(mut sdl_context: &sdl2::Sdl,
-           indexx: &mut i32,
+          indexx: &mut i32,
           alive: &mut bool) {
-
     let mut event_pump = sdl_context.event_pump().unwrap();
     while let Some(event) = event_pump.poll_event() {
         match event {
             Event::KeyDown {
                 keycode: Some(Keycode::Escape),
                 ..
-            } => { *alive = false; break; },
+            } => {
+                *alive = false;
+                break;
+            }
             Event::KeyDown {
                 keycode: Some(Keycode::A),
                 ..
@@ -175,8 +314,7 @@ fn update(mut sdl_context: &sdl2::Sdl,
 //     event_pump.push_event(custom_event).expect("Failed to push custom event");
 // }
 
-fn run(font_path: &Path, lines: &[&str]) -> Result<(), String> {
-
+fn run(font_path: &Path) -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsys = sdl_context.video()?;
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
@@ -192,35 +330,18 @@ fn run(font_path: &Path, lines: &[&str]) -> Result<(), String> {
 
     // Load a font
     let font = ttf_context.load_font(font_path, 160).unwrap();
-    // let canvas = Arc::new(Mutex::new(canvas_raw));
-    // let font_mutex = Arc::new(Mutex::new(font_raw));
-    // font_mutex.lock().unwrap().set_style(sdl2::ttf::FontStyle::BOLD);
-
-    // let canvas_mutex_thread = canvas_mutex.clone();
-    // let font_mutex_thread = font_mutex.clone();
-
-
-    // printooo(&mut canvas,lines, &font, 255);
-
-
-    // println!("Custom event ID: {}", custom_event_id);
-
 
     let event_subsystem = sdl_context.event()?;
     let event_subsystem_arc = Arc::new(Mutex::new(event_subsystem));
 
-//     let handle = thread::spawn(|| {
-//     });
-
-    // printooo(&mut canvas, lines, &font,  0);
-    // canvas.present();
-
     let mut indexx = 50;
     let mut last_frame_time = Instant::now();
-    let mut alive= true;
+    let mut alive = true;
 
+    let dbl = DashBoard::new();
+    let mut index_f = 0;
     loop {
-        if !alive{
+        if !alive {
             break;
         }
         // Calculate the elapsed time since the last frame
@@ -229,24 +350,32 @@ fn run(font_path: &Path, lines: &[&str]) -> Result<(), String> {
         // Update your game logic here...
         update(&sdl_context, &mut indexx, &mut alive);
 
-        if indexx< 10 {
+        if indexx < 10 {
             indexx = 255;
         }
-        indexx-=10;
+        indexx -= 10;
+
+        dbl.update_content();
+        let lines = dbl.get_content();
 
         // Render your game here...
-        printooo(&mut canvas, lines, &font, indexx);
+        printooo(&mut canvas, &lines, &font, indexx);
         canvas.present();
 
         // Calculate the remaining time to reach the target frame time
         let remaining_time = FRAME_TIME.checked_sub(elapsed);
 
         // If there's remaining time, sleep for that duration
+        // print!("\r");
         if let Some(remaining) = remaining_time {
             thread::sleep(remaining);
         }
-        let aa = 1000  / remaining_time.unwrap().as_millis();
-        println!("{aa} FPS");
+        let aa = 1000 / remaining_time.unwrap().as_millis();
+
+        print!("\rframe: {index_f} fps:{aa}");
+        io::stdout().flush();
+        index_f += 1;
+
         // Set the last_frame_time to the current time to measure the next frame duration
         last_frame_time = Instant::now();
     }
@@ -266,13 +395,8 @@ fn main() -> Result<(), String> {
         println!("Usage: ./demo font.[ttf|ttc|fon]")
     } else {
         let path: &Path = Path::new(&args[1]);
-        let lines = vec![
-            "T2 Tief: 10 min",
-            "T3: 10 m",
-            "89 Altstatten: 10 m",
-            "83: 10 m",
-        ];
-        run(path, &lines)?;
+
+        run(path)?;
     }
 
     Ok(())
