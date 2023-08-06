41eaa13f14b6cc4f910ff62d6b1c9f4d53deda41
Hugo VERJUS <hverjus@nvidia.com>
2023-08-06 20:34:09 +0200

    added almost working version of the tool

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,13 @@ use std::thread;
 use std::ptr;
 use std::sync::{Arc, Mutex};
 use sdl2::sys::SDL_EventType;
-use std::time::{Duration, Instant};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::io::{self, Write};
 use std::string::ToString;
 use reqwest::Error;
 use sdl2::libc::time;
 use serde::{Deserialize, Serialize};
+use tokio::runtime::Runtime;
 
 const FPS: u32 = 30;
 const FRAME_TIME: Duration = Duration::from_micros((1_000_000 / FPS) as u64);
@@ -35,8 +36,8 @@ struct Connection {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Departure {
-    departure: DateTime<FixedOffset>,
-    delay: i32,
+    departureTimestamp: u64,
+    delay: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -54,6 +55,7 @@ macro_rules! rect (
 trait Printable {
     fn get_text(&self) -> String;
     fn get_color(&self) -> [u8; 3];
+    fn update_text_field(&mut self);
 }
 
 struct CustomEventData {
@@ -71,8 +73,19 @@ struct URLRequest {
 }
 
 struct URLResult {
-    timestamp: Instant,
-    delay: Duration
+    timestamp: SystemTime,
+    delay: Duration,
+}
+
+impl URLResult {
+    fn new(departure: u64, delay: u32) -> URLResult {
+        let duration_since_epoch = Duration::from_secs(departure);
+        let instant = UNIX_EPOCH + duration_since_epoch;
+        URLResult {
+            timestamp: instant,
+            delay: Duration::from_secs(delay as u64 * 60),
+        }
+    }
 }
 
 // from=Lausanne&to=Gen%C3%A8ve&fields[]=connections/from/departure&fields[]=connections/from/delay&limit=5
@@ -84,31 +97,36 @@ struct DashBoardLine {
 struct DashBoardBusLine {
     request_content: URLRequest,
     result_list: Vec<URLResult>,
-    last_update: Instant,
+    line: String,
+    basename: String,
+    last_update: SystemTime,
     color: [u8; 3],
 }
 
 
-impl DashBoardBusLine{
-    fn new(begin_station:String, end_station:String ) -> DashBoardBusLine{
-        DashBoardBusLine{
-            request_content: URLRequest{
+impl DashBoardBusLine {
+    fn new(begin_station: String, end_station: String, base_name: String) -> DashBoardBusLine {
+        DashBoardBusLine {
+            request_content: URLRequest {
                 begin_station,
                 end_station,
                 fields: vec![
-                    "connections/from/departure".to_string(),
+                    "connections/from/departureTimestamp".to_string(),
                     "connections/from/delay".to_string(),
                 ],
-                limit: 10
+
+                limit: 10,
             },
+            line: "Not set".to_string(),
+            basename: base_name,
             result_list: vec![],
-            last_update: Instant::now() - Duration::from_secs(3600),
-            color: [255,255,255],
+            last_update: SystemTime::now() - Duration::from_secs(3600),
+            color: [255, 255, 255],
         }
     }
 
-    async fn update(&self){
-        if self.last_update + Duration::from_secs(600) < Instant::now() {
+    async fn update(&mut self) {
+        if self.last_update + Duration::from_secs(600) < SystemTime::now() {
             return;
         }
         let b_sta = &self.request_content.begin_station;
@@ -116,35 +134,35 @@ impl DashBoardBusLine{
         let limit = &self.request_content.limit;
         let mut ss = format!("{URL_SBB}from={b_sta}&to={e_sta}&limit={limit}");
         for elm in &self.request_content.fields {
-            ss += format!("&fields[]={elm}").as_str();
+            ss += &format!("&fields[]={elm}").as_str();
         }
-        let resa = reqwest::get(ss);
+        let resa = reqwest::get(&ss);
         let res = resa.await;
-        if res.is_err(){
+        if res.is_err() {
             print!("Error sending GET request: {ss}");
             return;
         }
         let res_text = res.unwrap().text().await;
-        if res_text.is_err(){
+        if res_text.is_err() {
             print!("Error getting text from GET request: {ss}");
             return;
         }
 
         let text_to_parse = res_text.unwrap();
-        let conn_res = serde_json::from_str(text_to_parse);
+        let conn_res = serde_json::from_str(text_to_parse.as_str());
         if conn_res.is_err() {
             return;
-
         }
         let conn: Connections = conn_res.unwrap();
 
-        conn.connections.map
+        let url_results: Vec<URLResult> = conn.connections.iter()
+            .map(|c|
+                URLResult::new(c.from.departureTimestamp, c.from.delay))
+            .collect();
 
-
-
+        self.result_list = url_results;
+        self.last_update = SystemTime::now();
     }
-
-
 }
 
 impl Printable for DashBoardLine {
@@ -154,51 +172,106 @@ impl Printable for DashBoardLine {
     fn get_color(&self) -> [u8; 3] {
         self.color.clone()
     }
+    fn update_text_field(&mut self) {}
+}
+
+impl Printable for DashBoardBusLine {
+    fn get_text(&self) -> String {
+        self.line.clone()
+    }
+
+    fn get_color(&self) -> [u8; 3] {
+        self.color.clone()
+    }
+    fn update_text_field(&mut self) {
+        let mut rt = Runtime::new().unwrap();
+        let future = self.update();
+        let result = rt.block_on(future);
+
+        let now = SystemTime::now();
+        let mut index = 0;
+        for rr in &self.result_list {
+            if now > rr.timestamp + rr.delay {
+                break;
+            }
+            index += 1;
+        }
+        let bn = &self.basename;
+        if index >= self.result_list.len() {
+            self.line = format!("{bn}: update req!");
+            return;
+        }
+        let current_rr = &self.result_list[index];
+        let diff_dur_res = current_rr.timestamp.duration_since(now);
+        if diff_dur_res.is_err() {
+            self.line = format!("{bn}: invalid time!");
+            return;
+        }
+        let diff_dur = diff_dur_res.unwrap();
+        let minutes = diff_dur.as_secs() / 60;
+        let seconds = diff_dur.as_secs() % 60;
+        self.line = format!("{bn}: {minutes}:{seconds}");
+    }
 }
 
 struct DashBoardPage {
-    lines: [dyn Printable; 4],
+    lines: Vec<Box<dyn Printable>>,
+}
+
+impl DashBoardPage {
+    fn new() -> DashBoardPage {
+        DashBoardPage { lines: vec![] }
+    }
+
+    fn add_sbb_line(&mut self, base_name: String, begin: String, end: String) {
+        let line = DashBoardBusLine::new(begin, end, base_name);
+        self.lines.push(Box::new(line));
+    }
 }
 
 struct DashBoard {
     pages: Vec<DashBoardPage>,
+    curr_page: usize,
 }
 
-#[tokio::main]
-async fn make_request() -> Result<(), Error> {
-    let response = reqwest::get("https://google.com").await?;
-    let body = response.text().await?;
-    println!("{}", body);
-    Ok(())
-}
 
 impl DashBoard {
     fn new() -> DashBoard {
         DashBoard {
-            pages: vec![]
+            pages: vec![],
+            curr_page: 0,
         }
     }
 
-    fn update(mut self, line_num: i32, text: String) {
-        match line_num {
-            1 => { self.line1 = text; }
-            2 => { self.line2 = text; }
-            3 => { self.line3 = text; }
-            4 => { self.line4 = text; }
-            _ => { println!("Bad line number! No effect.") }
+    fn add_page(&mut self, p: DashBoardPage) {
+        self.pages.push(p);
+    }
+
+    fn update_content(&mut self) {
+        let mut page = &mut self.pages[self.curr_page];
+        let lines = &mut page.lines;
+        for i in 0..4 {
+            lines[i].update_text_field();
         }
     }
 
     fn get_content(&self) -> Vec<String> {
         let mut vec = Vec::new();
-        vec.push(self.line1.clone());
-        vec.push(self.line2.clone());
-        vec.push(self.line3.clone());
-        vec.push(self.line4.clone());
+        if self.curr_page >= self.pages.len() {
+            let cp = self.curr_page;
+            for i in 0..4 {
+                vec.push(format!("page {cp} missing"));
+            }
+            return vec;
+        }
+        let page: &DashBoardPage = &self.pages[self.curr_page];
+        let lines = &page.lines;
+        for i in 0..4 {
+            let todisp = lines[i].get_text();
+            vec.push(todisp);
+        }
         return vec;
     }
-
-    fn update_content(&self) {}
 }
 
 // Scale fonts to a reasonable size when they're too big (though they might look less smooth)
@@ -335,10 +408,34 @@ fn run(font_path: &Path) -> Result<(), String> {
     let event_subsystem_arc = Arc::new(Mutex::new(event_subsystem));
 
     let mut indexx = 50;
-    let mut last_frame_time = Instant::now();
+    let mut last_frame_time = SystemTime::now();
     let mut alive = true;
 
-    let dbl = DashBoard::new();
+    let mut dbl: DashBoard = DashBoard::new();
+    let mut page: DashBoardPage = DashBoardPage::new();
+
+    page.add_sbb_line(
+        "T2|HB".to_string(),
+        "Zurich, Freihofstrasse".to_string(),
+        "Zurich, Letzigrund".to_string(),
+    );
+    page.add_sbb_line(
+        "T3|HB".to_string(),
+        "Zurich, Siemens".to_string(),
+        "Zurich, Hubertus".to_string(),
+    );
+    page.add_sbb_line(
+        "89|Alt".to_string(),
+        "Zurich, Kappeli".to_string(),
+        "Zurich, Letzipark West".to_string(),
+    );
+    page.add_sbb_line(
+        "89|Oer".to_string(),
+        "Zurich, Albisrank".to_string(),
+        "Zurich, Oerlikon".to_string(),
+    );
+
+    dbl.add_page(page);
     let mut index_f = 0;
     loop {
         if !alive {
@@ -361,27 +458,30 @@ fn run(font_path: &Path) -> Result<(), String> {
         // Render your game here...
         printooo(&mut canvas, &lines, &font, indexx);
         canvas.present();
+        if elapsed.is_err() {
+            println!("Err with system time");
+            thread::sleep(Duration::from_secs(1));
+        } else {
+            // Calculate the remaining time to reach the target frame time
+            let remaining_time = FRAME_TIME.checked_sub(elapsed.unwrap());
 
-        // Calculate the remaining time to reach the target frame time
-        let remaining_time = FRAME_TIME.checked_sub(elapsed);
+            // If there's remaining time, sleep for that duration
+            // print!("\r");
+            if let Some(remaining) = remaining_time {
+                thread::sleep(remaining);
+            }
+            let aa = 1000 / remaining_time.unwrap().as_millis();
 
-        // If there's remaining time, sleep for that duration
-        // print!("\r");
-        if let Some(remaining) = remaining_time {
-            thread::sleep(remaining);
+            print!("\rframe: {index_f} fps:{aa}");
         }
-        let aa = 1000 / remaining_time.unwrap().as_millis();
 
-        print!("\rframe: {index_f} fps:{aa}");
         io::stdout().flush();
+
         index_f += 1;
 
         // Set the last_frame_time to the current time to measure the next frame duration
-        last_frame_time = Instant::now();
+        last_frame_time = SystemTime::now();
     }
-
-
-    // handle.join().unwrap();
 
     Ok(())
 }
